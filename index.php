395b7ede90028a832df1133fdf84158e9c82ee6a
José Areia <jose.apareia@gmail.com>
2021-01-27 12:55:00 +0000

    Atualização da home page

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
                         <h1 class="text-white font-weight-bold" id="hero-title">Conheça a Meiricarro,<br>o seu reparador citroën de Leiria.</h1>
                         <p class="text-white pt-3 pb-3" id="hero-paragraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua consectetur.</p>
                         <div class="button-div">
-                            <a href="#" class="text-decoration-none">
+                            <a href="#info-empresa" class="text-decoration-none">
                                 <button type="button" name="button" id="button-saber">Saber mais</button>
                             </a>
                             <a href="#" class="text-decoration-none">
@@ -62,11 +62,100 @@
 
         <div id="container">
             <div id="main">
-                CONTENT
+                <!-- Info Empresa -->
+                <div class="h-100 container" id="info-empresa">
+                    <h4 class="text-center font-weight-bold">Fique a saber um pouco mais sobre nós.</h4>
+                    <div class="d-flex justify-content-center mt-3">
+                        <p class="text-center" id="info-empresa-paragraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore. Magna aliqua, consectetur adipisicing elit eiusmod.</p>
+                    </div>
+                    <div class="row justify-content-around" id="info-empresa-cards">
+                        <div class="col-md-3">
+                            <div class="circle d-flex justify-content-around align-items-center">
+                                <i class="fas fa-cogs text-white"></i>
+                            </div>
+                            <p class="text-center mt-4">Maxime nemo delectus</p>
+                            <p class="text-center mt-3">Autem at nisi fuga harum. Perspiciatis quaerat amet perferendis ullam eveniet est.</p>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="circle d-flex justify-content-around align-items-center">
+                                <i class="fas fa-car text-white"></i>
+                            </div>
+                            <p class="text-center mt-4">Quis dolorem voluptatibus</p>
+                            <p class="text-center mt-3">Autem at nisi fuga harum. Perspiciatis quaerat amet perferendis ullam eveniet est.</p>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="circle d-flex justify-content-around align-items-center">
+                                <i class="far fa-calendar-alt text-white"></i>
+                            </div>
+                            <p class="text-center mt-4">Autem at nisi</p>
+                            <p class="text-center mt-3">Autem at nisi fuga harum. Perspiciatis quaerat amet perferendis ullam eveniet est.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Contactos Empresa -->
+                <div id="info-contactos">
+                    <div class="h-100 container">
+                        <div class="row">
+                            <div class="col-md-6">
+                                MAPA
+                            </div>
+                            <div class="col-md-6">
+                                <h4 class="font-weight-bold ml-3">Quer saber como pode chegar até a Meiricarro?</h4>
+                                <div class="mt-4 ml-3" id="info-contactos-paragraph">
+                                    <p>Iusto aut cupiditate harum iusto incidunt aut qui blanditiis. Quis dignissimos similique dicta quae laboriosam.</p>
+                                    <p>Qui iure qui veritatis vitae incidunt. Tenetur ut reiciendis et error dolore voluptate perferendis.</p>
+                                </div>
+                                <a href="#" class="text-decoration-none">
+                                    <button type="button" name="button" id="button-info-contactar" class="ml-3 mt-3">Escreva um e-mail</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Info Empresa -->
+                <div class="h-100 container" id="info-noticias">
+                    <h4 class="text-center font-weight-bold">Descubra todas as novidades que a Citroën têm para si!</h4>
+                    <div class="d-flex justify-content-center mt-3">
+                        <p class="text-center" id="info-noticias-paragraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore. Magna aliqua, consectetur adipisicing elit eiusmod.</p>
+                    </div>
+                    <div class="container-news">
+                        <div class="row">
+                            <div class="col-md-8">
+                                <div class="card-news" id="card-news1">
+                                    <p class="text-white">Id autem et sunt aut nostrum non expedita. Molestiae voluptatum sed nostrum voluptas eos. Accusantium ratione nemo consequatur et sed ex dolores ut.</p>
+                                    <p class="text-white">29 Maio 2020</p>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="card-news" id="card-news2">
+                                    <p class="text-white">Molestiae voluptatum sed nostrum voluptas eos accusantium ratione.</p>
+                                    <p class="text-white">29 Maio 2020</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mt-4">
+                            <div class="col">
+                                <div class="card-news" id="card-news3">
+                                    <p class="text-white">Molestiae voluptatum sed nostrum voluptas eos accusantium ratione. Voluptates aut beatae.</p>
+                                    <p class="text-white">29 Maio 2020</p>
+                                </div>
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-news" id="card-news4">
+                                    <p class="text-white">Inventore officia vel repudiandae quaerat in libero. Similique et qui et. Maiores dicta qui deleniti iusto iste perferendis ab. Molestiae laudantium quo temporibus ut fuga vel corporis.</p>
+                                    <p class="text-white">29 Maio 2020</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     <?php
         require "partials/footer.php";
     ?>
+    <script src="/resources/js/master.js"></script>
     </body>
 </html>
--- partials/header.php
+++ partials/header.php
@@ -11,7 +11,12 @@
     <!-- Favicon -->
     <!-- <link rel="shortcut icon" type="image/png" href="../resources/media/favicon.png"> -->
 
+    <!-- Fontawesome core CSS -->
+    <link href="/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
     <!-- Google Fonts (Raleway) -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700;800&display=swap" rel="stylesheet">
 
     <!-- CSS Links and Bootstrap Core -->
